d8d6924dcce397669bb2af8636126a68ef4650c3
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2023-06-11 14:16:25 -0300

    definicao das entidades de Playlist e Song

--- src/main/java/imd/ufrn/br/purposesong/entity/Playlist.java
+++ src/main/java/imd/ufrn/br/purposesong/entity/Playlist.java
@@ -1,4 +1,9 @@
 package imd.ufrn.br.purposesong.entity;
 
+import java.util.List;
+
 public class Playlist extends ModelDatabaseEntity {
+    List<Song> songs;
+    String name;
+    String userId;
 }
--- src/main/java/imd/ufrn/br/purposesong/entity/Song.java
+++ src/main/java/imd/ufrn/br/purposesong/entity/Song.java
@@ -1,4 +1,9 @@
 package imd.ufrn.br.purposesong.entity;
 
 public class Song extends ModelDatabaseEntity {
+    String path;
+    String name;
+    String artist;
+    String photo;
+    String hashFile;
 }
